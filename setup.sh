c99ea24396db33efa4f6327b50a8e61f37dd6415
ItsTamzid <itstamzid1@gmail.com>
2026-05-24 22:47:28 +0600

    setup.sh

--- setup.sh
+++ setup.sh
@@ -2,4 +2,6 @@
 echo "Installing SITE-BUSTER dependencies..."
 pkg install python
 pip install requests
+echo "alias SITE-BUSTER='cd ~/SITE-BUSTER && bash setup.sh'" >> ~/.SITE-BUSTER_short_cut
+
 python script.py
